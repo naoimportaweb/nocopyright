c6c9e917562d97666a8347ca03ab9ee26047a916
nao.importa.web <nao.importa.web@gmail.com>
2025-05-05 12:18:34 -0300

    Melhoria

--- install.sh
+++ install.sh
@@ -9,6 +9,11 @@ pip3 install argostranslate
 #pip3 install googletrans
 pip3 install opencv-python
 pip3 install gTTS googletrans==4.0.0-rc1 pygame
+
+pip3 install -U stable-ts
+pip3 install requests
+pip3 install faster-whisper
+
 #pip3 install moviepy
 # pip install moviepy==1.0.3
 
