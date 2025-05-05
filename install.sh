ef8450fffa81e46c1dd3c9367ccb9bcbbfd24af8
nao.importa.web <nao.importa.web@gmail.com>
2025-05-05 13:18:56 -0300

    Melhoria

--- install.sh
+++ install.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-sudo apt install gh
+# Execute no terminal como super usuario
+# sudo apt update -y
+# sudo apt install python3-pip -y
+# sudo apt install ffmpeg -y
+
+# Execute abaixo como usuário comum
+python3 -m pip config set global.break-system-packages true
 
 pip3 install IPython
 pip3 install kokoro
 pip3 install soundfile
 pip3 install argostranslate
-#pip3 install googletrans
 pip3 install opencv-python
 pip3 install gTTS googletrans==4.0.0-rc1 pygame
 
@@ -14,15 +19,25 @@ pip3 install -U stable-ts
 pip3 install requests
 pip3 install faster-whisper
 
-#pip3 install moviepy
-# pip install moviepy==1.0.3
-
 if [ ! -d $HOME/desenv ]
 then
-    mdkir $HOME/desenv
-    cd $HOME/desenv
-    gh repo clone naoimportaweb/nocopyright
+    mkdir $HOME/desenv
+    wget -O /tmp/nocopiright.zip https://codeload.github.com/naoimportaweb/nocopyright/zip/refs/heads/main
+    cd /tmp/
+    unzip nocopiright.zip
 
-    cd $HOME/desenv/nocopyright
-    python3 ./api/argoshelp.py
+    cd $HOME/desenv
+    mkdir $HOME/desenv/nocopyright
+    cp -r /tmp/nocopyright-main/* $HOME/desenv/nocopyright
+    python3 $HOME/desenv/nocopyright/api/argoshelp.py
+fi
+
+if [ ! -d $HOME/cursos ]
+then
+    mkdir $HOME/cursos
+fi
+
+if [ ! -d $HOME/tmp ]
+then
+    mkdir $HOME/tmp
 fi
